5b41d229f4ea31b38a6e95b3e689f3f1ca806ae9
israel <isotog4@miumg.edu.gt>
2026-03-15 22:23:54 -0600

    Se crea el archivo de la puerta logica

--- Tarea_Puerta_Logica/TareaPuertaLogica.cpp
+++ Tarea_Puerta_Logica/TareaPuertaLogica.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+
 using namespace std;
 
 int main() {
@@ -27,15 +29,17 @@ int main() {
     cout << "Pertenece al club de programacion? (1 = Si, 0 = No): ";
     cin >> club;
 
-    // Primera regla obligatoria
+    cout << fixed << setprecision(2);
+    cout << "Promedio ingresado: " << promedio << endl;
+
     if(edad < 18){
         cout << "Acceso denegado." << endl;
     }
     else{
 
-        if( (club == 1 && promedio >= 75) || 
-            (club == 0 && promedio >= 85) || 
-            (promedio >= 95) ){
+        if((club == 1 && promedio >= 75) ||
+           (club == 0 && promedio >= 85) ||
+           (promedio >= 95)){
             
             cout << "Acceso autorizado." << endl;
         }
